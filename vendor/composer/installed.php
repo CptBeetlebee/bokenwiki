68c49d572db9e91e010c11d144f0a217852f73ee
CptBeetlebee <90175503+CptBeetlebee@users.noreply.github.com>
2022-03-02 03:50:31 -0600

    Install Composer + other shit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'd67c336e36dd3166002e9ae8e87dbb52358487af',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'd67c336e36dd3166002e9ae8e87dbb52358487af',
+            'dev_requirement' => false,
+        ),
+    ),
+);
