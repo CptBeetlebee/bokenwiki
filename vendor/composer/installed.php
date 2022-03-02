e404d257ae61fa70bebae7a7f56b5f6349332c72
CptBeetlebee <90175503+CptBeetlebee@users.noreply.github.com>
2022-03-02 04:10:37 -0600

    correct version 2.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'ee0c60b116288f045cec13bc67d42e4d9b90633e',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'ee0c60b116288f045cec13bc67d42e4d9b90633e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
